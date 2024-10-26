ee766dbaaf5c601934f4b59e35237ca28be8d9d9
Ajay Kushwaha <132126378+Ajay8659@users.noreply.github.com>
2024-10-26 20:09:41 +0530

    Add files via upload

---
+++ Netflix/script.js
@@ -0,0 +1,69 @@
+function togglehide1() {
+    let box = document.getElementById("container1");
+    if (box.style.display === "none") {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+
+}
+
+function togglehide2() {
+    let box = document.getElementById("container2");
+    if (box.style.display === "none") {
+        box.style.display = "block"
+    } else {
+        box.style.display = "none";
+    }
+
+}
+function togglehide3() {
+    let box = document.getElementById("container3");
+    if (box.style.display === "none") {
+        box.style.display = "block"
+    } else {
+        box.style.display = "none";
+    }
+
+}
+function togglehide4() {
+    let box = document.getElementById("container4");
+    if (box.style.display === "none") {
+        box.style.display = "block"
+    } else {
+        box.style.display = "none";
+    }
+
+}
+function togglehide5() {
+    let box = document.getElementById("container5");
+    if (box.style.display === "none") {
+        box.style.display = "block"
+    } else {
+        box.style.display = "none";
+    }
+
+}
+function togglehide6() {
+    let box = document.getElementById("container6");
+    if (box.style.display === "none") {
+        box.style.display = "block"
+    } else {
+        box.style.display = "none";
+    }
+
+}
+
+function sliderReverse() {
+    let slider = document.getElementById("slider-cont");
+    slider.style.transform = "translate(-770px)";
+}
+function sliderReverse() {
+    let slider = document.getElementById("slider-cont");
+    slider.style.transform = "translate(-770px)";
+}
+
+function sliderfarward() {
+    let slider = document.getElementById("slider-cont");
+    slider.style.transform = "translate(0px)";
+}

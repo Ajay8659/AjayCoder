05eb400d8a005d6f07e5544e09d73f27bfa71ef5
Ajay Kushwaha <132126378+Ajay8659@users.noreply.github.com>
2025-04-17 12:22:06 +0530

    Add files via upload

---
+++ scm2.0/scm2.0/src/test/java/com/icoderhub/scm20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icoderhub.scm20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
